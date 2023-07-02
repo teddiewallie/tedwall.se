88a0d3b964daa8073f1f1a9d363e0d2c2bf3459f
Ted Wall <ted@tedwall.se>
2023-07-02 15:42:52 +0200

    v1

--- back/src/app.controller.ts
+++ back/src/app.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('api')
+@Controller('api/v1')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getV1(): string {
+    return this.appService.getV1();
   }
 }
