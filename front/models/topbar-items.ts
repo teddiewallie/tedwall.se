3e31a56edb130184779bee7603c3bb89aaa31ee3
Ted Wall <ted@tedwall.se>
2023-07-03 19:08:54 +0200

    emoji

--- front/models/topbar-items.ts
+++ front/models/topbar-items.ts
@@ -1,9 +1,9 @@
 const topbarItems = {
-  Home: '/',
-  Blog: '/blog',
-  Styleguide: '/styleguide',
-  'About Me': '/about-me',
-  GitHub: 'https://github.com/teddiewallie/tedwall.se'
+  '🏠 Home': '/',
+  '📝 Blog': '/blog',
+  '🎨 Styleguide': '/styleguide',
+  '🧑 About Me': '/about-me',
+  '👨‍💻 GitHub': 'https://github.com/teddiewallie/tedwall.se'
 };
 
 export default topbarItems;
