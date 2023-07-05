0d3e303ad1ee58f4d8c2223f61492a4b972505c0
Ted Wall <ted@tedwall.se>
2023-07-05 22:48:25 +0200

    mobile

--- front/components/article.tsx
+++ front/components/article.tsx
@@ -6,13 +6,12 @@ import { Newsreader } from 'next/font/google';
 const font = Newsreader({ subsets: ['latin'] });
 
 const Wrapper = styled.article`
-  min-width: 20em;
+  min-width: 10em;
   max-width: 40em;
   padding-left: 1em;
   padding-right: 2vw;
   padding-top: 1em;
-  line-height: 1.6em;
-  font-size: 1.1em;
+  line-height: 1.4em;
 `;
 
 const Article = (props: {children: React.ReactNode}) => {
