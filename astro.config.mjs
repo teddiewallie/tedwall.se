991ff9cbf8d3d37adc88da120abde3499ec586e0
Ted <no@nope>
2026-03-22 17:07:32 +0100

    self host

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,6 +17,7 @@ export default defineConfig({
                         { label: 'Thought from Terminal', slug: 'writeups/thought-from-terminal' },
                         { label: 'My Keyboard Layout', slug: 'writeups/my-keyboard-layout' },
                         { label: 'My Arch Mirror', slug: 'writeups/my-arch-mirror' },
+                        { label: 'Local Documentation', slug 'writeups/local-documentation' },
                     ],
                 },
 				{
